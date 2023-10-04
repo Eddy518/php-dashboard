668a3035fc2904716d9e2059429a76d30178b507
Eddy518 <edmwangi2@gmail.com>
2023-10-04 21:33:34 +0300

    feat: Initial commit

---
+++ controllers/about.php
@@ -0,0 +1,9 @@
+<?php
+
+$heading = "About us";
+
+require_once '../views/about.view.php';
+
+die() && debugger_print();
+// Controllers receive request and return response
+// E.g. User requests to visit about.php and the response is require "views/about.view.php"; which is the about view or html
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require "functions.php";
+
+$uri = $_SERVER['REQUEST_URI'];
+if($uri === '/'){
+    require ('controllers/index.php');
+} elseif ($uri === '/about'){
+    require ('controllers/about.php');
+} elseif($uri === '/contact'){
+    require ('controllers/contact.php');
+}
+
